2e8e761315cfce31133ad0075b1ac2c3e6c348b6
Ufuk Ayyıldız <23309320+ufukayyildiz@users.noreply.github.com>
2025-05-10 00:58:40 +0300

    Update index.ts

--- template/src/index.ts
+++ template/src/index.ts
@@ -2,9 +2,9 @@ import { R2Explorer } from "r2-explorer";
 
 // Read all available configurations here: https://r2explorer.dev/getting-started/configuration/
 export default R2Explorer({
-	readonly: true,
-	// basicAuth: {
-	//   username: 'username',
-	//   password: 'password'
-	// }
+	readonly: false,
+basicAuth: {
+username: 'ufuk',
+password: 'sikenufuk123'
+}
 });
